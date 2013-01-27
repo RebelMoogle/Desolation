9a91b2265ba9c0bcd84f419ba82cfaca97054d47
kkkk <sworden@students.aie.edu.au>
2013-01-27 12:13:56 -0800

    Main menu functionality

--- GameJam/Assets/Project/Scripts/GUIControl.js
+++ GameJam/Assets/Project/Scripts/GUIControl.js
@@ -1,9 +1,23 @@
 #pragma strict
 
-function Start () {
+private var hit : RaycastHit;
+private var ray : Ray;
+ 
+function FixedUpdate () {
 
-}
+    if( Input.GetMouseButtonDown(0) ) {
+    
+        ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        Debug.DrawLine( ray.origin, ray.direction * 10 );
+        if( Physics.Raycast(ray.origin, ray.direction * 10, hit )){
+        	
+        	if( hit.transform.name == "gui_button_start" )
+        	{
+        		Debug.Log("Started Game!");
+        		Application.LoadLevel("levelOne");
+        	}
 
-function Update () {
-
-}
+        }
+        
+    }
+} 
