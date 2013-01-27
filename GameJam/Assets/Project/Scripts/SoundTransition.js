ee4b15352e4c886b7a62fe88529ee637314b02d6
kkkk <sworden@students.aie.edu.au>
2013-01-26 18:43:02 -0800

    commitz

--- GameJam/Assets/Project/Scripts/SoundTransition.js
+++ GameJam/Assets/Project/Scripts/SoundTransition.js
@@ -1,48 +1,43 @@
 #pragma strict
 
-public var audioSource1 : AudioSource;
-public var audioSource2 : AudioSource;
+public var MonsterObject : GameObject;
 
-// Time to wait before repeating the loop.
-public var waitTime : float;
+public var musicSource1 : AudioSource;
+public var musicSource2 : AudioSource;
 
-// Number of times to loop.
-public var stepNum : int;
+public var heartbeatSource : AudioSource;
 
+public var minHeartDist : float;
+public var musicChangeDist : float;
 
+private var monsterDist : float;
 
 //===============================
 
 function Start () {
-		
-	audioSource1.volume = 1.0;
-	audioSource1.loop = true;
-	audioSource1.Play();
-
-	audioSource2.volume = 0.0;
-	audioSource2.loop = true;
-	audioSource2.Play();
-
-
-
-	ChangeMusic();
 	
+	musicSource1.volume = 1.0;
+	musicSource1.loop = true;
+
+	musicSource2.volume = 0.0;
+	musicSource2.loop = true;
+
 }
 
 //===============================
 
-function ChangeMusic () 
+function ChangeMusic ( waitTime : float, stepNum : int, a : boolean ) 
 {
-	var a : boolean;
+//	var a : boolean;
 	
-	if( audioSource1.volume == 1.0 )
-	{
-		a = true;
-	}
-	else
-	{
-		a = false;
-	}
+//	if( musicSource1.volume == 1.0 )
+//	{
+//		a = true;
+//	}
+//	else
+//	{
+//		a = false;
+//	}
 	
 	for ( var i : int = 1; i < (stepNum+1); i++ )
 	{
@@ -51,13 +46,15 @@ function ChangeMusic ()
 		
 		if( a )
 		{
-			audioSource1.volume = Mathf.Lerp(1.0, 0.0, percentage);
-			audioSource2.volume = Mathf.Lerp(0.0, 1.0, percentage);
+			// transition from music1 to music 2
+			musicSource1.volume = Mathf.Lerp(1.0, 0.0, percentage);
+			musicSource2.volume = Mathf.Lerp(0.0, 1.0, percentage);
 		}
 		else
 		{
-			audioSource1.volume = Mathf.Lerp(0.0, 1.0, percentage);
-			audioSource2.volume = Mathf.Lerp(1.0, 0.0, percentage);
+			// transition from music 2 to music 1
+			musicSource1.volume = Mathf.Lerp(0.0, 1.0, percentage);
+			musicSource2.volume = Mathf.Lerp(1.0, 0.0, percentage);
 		}
 		
 		var timer = Time.timeSinceLevelLoad + waitTime;
@@ -66,7 +63,71 @@ function ChangeMusic ()
     	}
 		
 	}
-	
+}
 
+function HeartBeat ()
+{
+	
+	if ( monsterDist <= minHeartDist )
+	{
+		
+		// Play heartbeat sound at a speed relative to distance
+		
+	}
+	
+	
 	
 }
+
+function ProximitySounds ()
+{
+	
+	if ( monsterDist < musicChangeDist && musicSource1.isPlaying == false )
+	{
+		musicSource1.Play();
+		musicSource2.Play();
+		ChangeMusic(0.3, 150, true);
+		
+//		yield WaitForSeconds(5.0); // Wait for Changemusic to do its thing..
+		
+	}	
+	else
+	{
+	
+		if( musicSource1.volume != 1.0 && musicSource1.isPlaying )
+		{
+			ChangeMusic(0.05, 500, false);
+//			yield WaitForSeconds(5.0); // Wait for Changemusic to do its thing..
+		}
+		else
+		{
+			musicSource1.Stop();
+			musicSource2.Stop();
+		}
+		
+	}
+	
+	
+}
+
+//===============================
+
+function Update ()
+{
+	
+	monsterDist = Vector3.Distance( transform.position, MonsterObject.transform.position );
+	
+	ProximitySounds();
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
